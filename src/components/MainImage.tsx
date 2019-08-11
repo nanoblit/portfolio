51682c53001587af8fef96003b98f8f43fe211c2
nanoblit <nanoblit@users.noreply.github.com>
2019-08-11 10:13:32 +0200

    Add my picture

--- src/components/MainImage.tsx
+++ src/components/MainImage.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import styled from "styled-components";
+import { mainColor } from "../styles/variables";
 
-import image from "../images/background.jpg";
+import background from "../images/background.jpg";
+import me from "../images/me.jpg";
 
 const MainImageDiv = styled.div`
   position: relative;
-  background-image: url(${image});
+  background-image: url(${background});
   background-size: cover;
   padding-top: 51.32%;
 
@@ -14,6 +16,19 @@ const MainImageDiv = styled.div`
     max-width: 50rem;
     top: 6rem;
     left: 6rem;
+  }
+
+  .me {
+    position: absolute;
+    max-width: 50rem;
+    top: 6rem;
+    right: 6rem;
+
+    img {
+      width: 20rem;
+      border: 2px solid ${mainColor};
+      border-radius: 100%;
+    }
   }
 `;
 
@@ -25,6 +40,9 @@ const MainImage = (): JSX.Element => {
           I am a full-stack web developer interested in modern web technologies
           who also enjoys gaming and gamedev.
         </p>
+      </div>
+      <div className="me">
+        <img alt="My face" src={me} />
       </div>
       <p>An awesome image with some text and my face</p>
     </MainImageDiv>
